5dd3704fcb97cd13eb227a6f88f40006a66c9bc6
Shulammite Lim <shulammitelim@gmail.com>
2020-09-26 00:44:42 -0500

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Shulammite Lim on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
