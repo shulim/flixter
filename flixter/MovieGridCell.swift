17db971af5432a0034c75a54520a6305849a4598
Shulammite Lim <shulammitelim@gmail.com>
2020-10-02 23:44:09 -0500

    finish collection view

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Shulammite Lim on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
